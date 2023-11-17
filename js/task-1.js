9d9e52e12b180404418c5f2a1f070f7dac5c0ede
valiana35 <144236658+valiana35@users.noreply.github.com>
2023-11-17 22:14:30 +0200

    jshw1

--- js/task-1.js
+++ js/task-1.js
@@ -3,10 +3,6 @@ function makeTransaction(quantity, pricePerDroid) {
     const totalPrice = quantity * pricePerDroid;
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
-
-makeTransaction(5, 3000);
 console.log(makeTransaction(5, 3000));
-makeTransaction(3, 1000);
 console.log(makeTransaction(3, 1000));
-makeTransaction(10, 500);
 console.log(makeTransaction(10, 500));
